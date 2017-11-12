2e74a49be927eda0dee599f7743d2a5d31dd7ddb
kosalaishan <kgkosalaishan@gmail.com>
2017-11-12 21:58:11 +0530

    wimie template with angular

---
+++ src/app/Home/home.component.ts
@@ -0,0 +1,12 @@
+import { AppComponent } from "../app.component";
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'home',
+    templateUrl:'./home.component.html'
+})
+
+export class HomeComponent{
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './Home/home.component';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
